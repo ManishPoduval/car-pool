90eb79002ea5ede94e0ad6f94e08e154ee86bc03
Manish Poduval <manishpoduval@hotmail.com>
2017-11-11 23:30:25 +0530

    formatted require-config

--- ShortestPath/src/js/require-config.js
+++ ShortestPath/src/js/require-config.js
@@ -1,20 +1,20 @@
 require.config({
-	paths: {
-		'jquery': '../../components/jquery/dist/jquery',
-		'jquery.cookie': '../../components/jquery.cookie/jquery.cookie',
-		'bootstrap': '../../components/bootstrap/dist/js/bootstrap.min',
-		'knockout': '../../components/knockout/build/output/knockout-latest',
-		'postal': '../../components/postal/lib/postal',
+    paths: {
+        'jquery': '../../components/jquery/dist/jquery',
+        'jquery.cookie': '../../components/jquery.cookie/jquery.cookie',
+        'bootstrap': '../../components/bootstrap/dist/js/bootstrap.min',
+        'knockout': '../../components/knockout/build/output/knockout-latest',
+        'postal': '../../components/postal/lib/postal',
         'lodash': '../../components/lodash/lodash',
         'json2': '../../components/json2/lib/json2/static/json2',
         'font-awesome': '../../components/font-awesome/css/font-awesome',
         'async': '../../components/async/dist/async'
-	},
-	shim: {
-        bootstrap : {
-            dep : [ 'jquery'],
+    },
+    shim: {
+        bootstrap: {
+            dep: ['jquery'],
             exports: 'bootstrap'
         },
-	},
-	deps: ['jquery', 'jquery.cookie', 'bootstrap', 'json2']
+    },
+    deps: ['jquery', 'jquery.cookie', 'bootstrap', 'json2']
 });
